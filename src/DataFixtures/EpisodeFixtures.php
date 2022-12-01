f24f362abe5125867f2fd7f1a971e2f2c4b75e9f
Kévin Albespy <kevin.albespy@gmail.com>
2022-12-01 11:53:42 +0100

    Quest 12: Radiated

--- src/DataFixtures/EpisodeFixtures.php
+++ src/DataFixtures/EpisodeFixtures.php
@@ -6,40 +6,30 @@ use App\Entity\Episode;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class EpisodeFixtures extends Fixture implements DependentFixtureInterface
 {
-    public const EPISODES = [
-        ['season' => 1, 'title' => 'episode 1', 'number' => 1, 'synopsis' => 'Eppisode 1 de season id 1'],
-        ['season' => 1, 'title' => 'episode 2', 'number' => 2, 'synopsis' => 'Eppisode 2 de season id 1'],
-        ['season' => 1, 'title' => 'episode 3', 'number' => 3, 'synopsis' => 'Eppisode 3 de season id 1'],
-        ['season' => 2, 'title' => 'episode 1', 'number' => 1, 'synopsis' => 'Eppisode 1 de season id 2'],
-        ['season' => 2, 'title' => 'episode 2', 'number' => 2, 'synopsis' => 'Eppisode 2 de season id 2'],
-        ['season' => 3, 'title' => 'episode 1', 'number' => 1, 'synopsis' => 'Eppisode 1 de season id 3'],
-        ['season' => 3, 'title' => 'episode 2', 'number' => 2, 'synopsis' => 'Eppisode 2 de season id 3'],
-        ['season' => 3, 'title' => 'episode 3', 'number' => 3, 'synopsis' => 'Eppisode 3 de season id 3'],
-        ['season' => 4, 'title' => 'episode 1', 'number' => 1, 'synopsis' => 'Eppisode 1 de season id 4'],
-        ['season' => 4, 'title' => 'episode 2', 'number' => 2, 'synopsis' => 'Eppisode 2 de season id 4'],
-        ['season' => 4, 'title' => 'episode 3', 'number' => 3, 'synopsis' => 'Eppisode 3 de season id 4'],
-        ['season' => 4, 'title' => 'episode 4', 'number' => 4, 'synopsis' => 'Eppisode 4 de season id 4'],
-    ];
 
     public function load(ObjectManager $manager): void
     {
-        foreach (self::EPISODES as $key => $programEpisode) {
+        $faker = Factory::create();
+
+        for ($i = 0; $i < 400; $i++) {
             $episode = new Episode();
-            $episode->setSeason($this->getReference('season_' . $programEpisode['season']));
-            $episode->setTitle($programEpisode['number']);
-            $episode->setNumber($programEpisode['number']);
-            $episode->setSynopsis($programEpisode['synopsis']);
+            $episode->setSeason($this->getReference('season_' . $faker->numberBetween(0, 10)));
+            $episode->setTitle($faker->words(3, true));
+            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setSynopsis($faker->paragraphs(1, true));
+
             $manager->persist($episode);
         }
+
         $manager->flush();
     }
 
     public function getDependencies()
     {
-        // Tu retournes ici toutes les classes de fixtures dont ProgramFixtures dépend
         return [
             SeasonFixtures::class,
         ];
--- src/DataFixtures/SeasonFixtures.php
+++ src/DataFixtures/SeasonFixtures.php
@@ -5,40 +5,38 @@ namespace App\DataFixtures;
 use App\Entity\Season;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Faker\Factory;
 use Doctrine\Persistence\ObjectManager;
 
 class SeasonFixtures extends Fixture implements DependentFixtureInterface
 {
-    public const SEASONS = [
-        ['program' => 1, 'number' => 1, 'year' => 2002, 'description' => 'C\'est la saison 1 de The Office'],
-        ['program' => 1, 'number' => 2, 'year' => 2003, 'description' => 'C\'est la saison 2 de The Office'],
-        ['program' => 1, 'number' => 3, 'year' => 2004, 'description' => 'C\'est la saison 3 de The Office'],
-        ['program' => 1, 'number' => 4, 'year' => 2005, 'description' => 'C\'est la saison 4 de The Office'],
-        ['program' => 1, 'number' => 5, 'year' => 2006, 'description' => 'C\'est la saison 5 de The Office'],
-        ['program' => 3, 'number' => 1, 'year' => 1998, 'description' => 'C\'est la saison 1 de The Wire'],
-        ['program' => 3, 'number' => 2, 'year' => 1999, 'description' => 'C\'est la saison 2 de The Wire'],
-        ['program' => 3, 'number' => 3, 'year' => 2000, 'description' => 'C\'est la saison 3 de The Wire'],
-        ['program' => 2, 'number' => 1, 'year' => 2010, 'description' => 'C\'est la saison 1 de Breaking Bad'],
-        ['program' => 4, 'number' => 1, 'year' => 2003, 'description' => 'C\'est la saison 1 de Malcolm'],
-        ['program' => 6, 'number' => 1, 'year' => 2014, 'description' => 'C\'est la saison 1 de Community'],
-        ['program' => 7, 'number' => 1, 'year' => 2002, 'description' => 'C\'est la saison 1 de Scrubs'],
-    ];
-
     public function load(ObjectManager $manager): void
     {
-        foreach (self::SEASONS as $key => $programSeason) {
+        //Puis ici nous demandons à la Factory de nous fournir un Faker
+        $faker = Factory::create();
+
+        /**
+         * L'objet $faker que tu récupère est l'outil qui va te permettre 
+         * de te générer toutes les données que tu souhaites
+         */
+
+        for ($i = 0; $i < 40; $i++) {
             $season = new Season();
-            $season->setProgram($this->getReference('program_' . $programSeason['program']));
-            $season->setNumber($programSeason['number']);
-            $season->setYear($programSeason['year']);
-            $season->setDescription($programSeason['description']);
+            //Ce Faker va nous permettre d'alimenter l'instance de Season que l'on souhaite ajouter en base
+            $season->setNumber($faker->numberBetween(1, 10));
+            $season->setYear($faker->year());
+            $season->setDescription($faker->paragraphs(1, true));
+
+            $season->setProgram($this->getReference('program_' . $faker->numberBetween(0, 7)));
+
             $manager->persist($season);
-            $this->addReference('season_' . $key, $season);
+            $this->addReference('season_' . $i, $season);
         }
+
         $manager->flush();
     }
 
-    public function getDependencies()
+    public function getDependencies(): array
     {
         // Tu retournes ici toutes les classes de fixtures dont ProgramFixtures dépend
         return [
