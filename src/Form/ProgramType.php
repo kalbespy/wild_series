3be0e81566aa3634134a5d90c95d123b1e131eba
Kévin Albespy <kevin.albespy@gmail.com>
2022-12-16 15:10:49 +0100

    Quest 19: EntityTyped

--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -6,6 +6,8 @@ use App\Entity\Program;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Entity\Actor;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class ProgramType extends AbstractType
 {
@@ -17,7 +19,14 @@ class ProgramType extends AbstractType
             ->add('poster')
             ->add('country')
             ->add('year')
-            ->add('category', null, ['choice_label' => 'name']);
+            ->add('category', null, ['choice_label' => 'name'])
+            ->add('actors', EntityType::class, [
+                'class' => Actor::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+                'by_reference' => false,
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
